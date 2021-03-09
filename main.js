24827fdec4599d376dc3dd0a2d2475417b826b29
Lorenzo De Sossi <lollodesossi@gmail.com>
2021-03-09 01:38:44 +0100

    funzione palindroma

---
+++ main.js
@@ -0,0 +1,14 @@
+var domanda = prompt("Inserisci una parola")
+
+var parola = convalida
+
+
+function palindroma(domanda) {
+  return domanda == domanda.split('').reverse('').join('');
+}
+
+if (palindroma = true) {
+  document.getElementById('palindroma').innerHTML = "Complimenti, la parola che hai inserito è palindroma"
+} else {   document.getElementById('palindroma').innerHTML = "Mi dispiace, la parola che hai inserito non è palindroma"
+
+}
